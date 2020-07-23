31aca94ac2f620f0691c0ddacfeb84f6a40e7f1b
unknown <viiktoor@gmail.com>
2020-07-23 14:49:30 -0400

    canAddFish() exercise complete

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,22 +1,45 @@
 import java.util.*
-import java.util.Random as Random
 
 fun main(args: Array<String>) {
     //println(if(args[0].toInt() <= 12) "Good morning, Kotlin" else "Good night, Kotlin")
-    feedTheFish()
+    //feedTheFish()
     //swim(50, speed = "slow")
+    println(canAddFish(10, listOf(3,3,3)))
+    println(canAddFish(8, listOf(2,2,2), hasDecorations = false))
+    println(canAddFish(9, listOf(1,3,3),3))
+    println(canAddFish(10, listOf(), 7, true))
+    println(canAddFish(50, listOf(7,8,9,5,4,10), 5, true))
 
 }
+
+fun canAddFish(
+    tankSize: Int,
+    currentFish: List<Int>,
+    fishSize: Int = 2,
+    hasDecorations: Boolean = true
+): Boolean {
+
+
+    val realTankSize = if (hasDecorations == true) tankSize * 80 / 100 else tankSize
+    val sumFishes = if (currentFish.isEmpty()) currentFish+1 else currentFish
+
+    return if (sumFishes.sum() >= realTankSize) {
+        false
+    } else if (currentFish.sum() == 0 && fishSize <= realTankSize){
+        true
+    }else fishSize <= realTankSize.rem(sumFishes.sum())
+}
+
 
 fun shouldChangeWater(
     day: String,
     temperature: Int = 22,
     dirty: Int = 20
-): Boolean{
+): Boolean {
     return true
 }
 
-fun swim (time : Int, speed : String = "fast"){
+fun swim(time: Int, speed: String = "fast") {
     println("swimming $speed at $time kph")
 }
 
@@ -25,7 +48,7 @@ fun feedTheFish() {
     val food = fishFood(day)
     println("today is $day and the fish eat $food")
 
-    if (shouldChangeWater(day)){
+    if (shouldChangeWater(day)) {
         println("change the water today")
     }
 }
@@ -38,7 +61,7 @@ fun randomDay(): String {
 fun fishFood(day: String): String {
     var food = "fasting"
 
-    return when(day){
+    return when (day) {
         "sunday" -> "flakes"
         "monday" -> "worms"
         "wednesday" -> "granules"
@@ -54,7 +77,7 @@ fun dayOfWeek() {
 
     val day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
 
-    when (day){
+    when (day) {
         1 -> println("today is sunday :)")
         2 -> println("today is monday :(")
         3 -> println("today is tuesday :(")
@@ -65,10 +88,10 @@ fun dayOfWeek() {
     }
 
     val temperature = 10
-    val isHot = if (temperature > 50 ) true else false
+    val isHot = if (temperature > 50) true else false
     //println(isHot)
 
-    val message = "you are ${if (temperature > 50)"fried" else "safe"} fish"
+    val message = "you are ${if (temperature > 50) "fried" else "safe"} fish"
     //println(message)
 
 }
---
+++ src/TestMath.kt
@@ -0,0 +1,8 @@
+fun main(){
+    println(10.rem(9))
+    println(8.rem(6))
+    println(9.rem(8)) // to calculate if the size of new fish is lower or equal the remainder
+
+    println(10*.8) // to calculate the size of tank fish if it has decorations or not
+    println(10*1)
+}
