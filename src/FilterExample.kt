38a589bcebb9f21f774d356a266bd685d7423df6
unknown <viiktoor@gmail.com>
2020-07-27 12:39:34 -0400

    lambdas

--- src/FilterExample.kt
+++ src/FilterExample.kt
@@ -3,8 +3,17 @@ fun main(){
     val spices = listOf("curry", "pepper", "cayenne", "ginger", "red curry", "green curry", "red pepper" )
 
 
-    println(spices.sortedWith(compareBy { it.length }))
-    println(spices.filter { it[0] == 'c'})
-    //println(spices.filter { it ->  })
+    println(spices.filter { it.contains("curry") }.sortedBy { it.length })  // right solution (the exercise asks to do what I've done but just with curries)
 
+    println(spices.filter { it.startsWith('c') && it.endsWith('e')})
+
+    println(spices.take(3).filter { it.startsWith('c') })
+
+    println(spices.sortedWith(compareBy { it.length })) // this works but it wasn't the idea of the exercise
+
+    val it: ListIterator<String> = spices.listIterator() // this works but it wasn't the idea of the exercise
+    while (it.hasNext()){
+        val e = it.next()
+        print("$e ,")
+    }
 }
---
+++ src/LambdaPractice.kt
@@ -0,0 +1,16 @@
+import kotlin.random.Random
+
+fun main (){
+
+    val rollDice = { Random.nextInt(12)}
+    val rollDice2: (Int) -> Int = {sides -> if (sides == 0) 0 else Random.nextInt(sides)}
+    val rollDice3 = { sides: Int -> if (sides != 0) {Random.nextInt(0,sides)} else 0 }
+
+//    rollDice()
+//    rollDice3(20)
+//    rollDice2(6)
+
+    fun gameplay(diceRoll: Int) {println(diceRoll)}
+
+    gameplay(rollDice2(4))
+}
